66b0c6907982a839b1e07c2f811f6f02047ba0f4
Dinesh <dinesh210805@gmail.com>
2025-06-02 13:24:49 +0530

    Add HelloWorld and MarkCalc Java classes

---
+++ JAVA/MarkCalc.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class MarkCalc {
+    String Name;
+    int m1,m2,m3,m4,m5;
+    
+    public MarkCalc() {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter the Student Name : ");
+        Name = read.nextLine();
+        System.out.print("M1 : ");
+        m1 = read.nextInt();
+        System.out.print("M2 : ");
+        m2 = read.nextInt();
+        System.out.print("M3 : ");
+        m3 = read.nextInt();
+        System.out.print("M4 : ");
+        m4 = read.nextInt();
+        System.out.print("M5 : ");
+        m5 = read.nextInt();
+        System.out.println("Hi!"+Name);
+        System.out.println("Your Total :"+(m1+m2+m3+m4+m5));
+        System.out.println("Avg :"+(m1+m2+m3+m4+m5)/5);
+    }
+    
+    public static void main(String[] args) {
+        MarkCalc obj = new MarkCalc();
+    }
+}
